e6538669cf0236ffded5a2c3d63a1ddd80c1aaec
Christopher Susandji <christopher.susandji@gmail.com>
2026-07-27 14:47:30 +0700

    Initial Commit

---
+++ FloatingTimer/FloatingTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingTimerApp.swift
+//  FloatingTimer
+//
+//  Created by Christopher Susandji on 27/07/26.
+//
+
+import SwiftUI
+
+@main
+struct FloatingTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
