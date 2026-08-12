d4edda2d3ef118557d0bb3f5393f0ee09f15820b
Christopher Susandji <christopher.susandji@gmail.com>
2026-08-12 17:27:56 +0700

    Update permission and Menu items

--- FloatingTimer/FloaterApp.swift
+++ FloatingTimer/FloaterApp.swift
@@ -10,14 +10,27 @@ import SwiftUI
 @main
 struct FloaterApp: App {
     @State private var appViewModel = AppViewModel()
+    @NSApplicationDelegateAdaptor private var appDelegate: FloaterAppDelegate
     
     var body: some Scene {
         WindowGroup("Floater") {
             ContentView()
                 .environment(appViewModel)
                 .frame(width: 300)
+                .onAppear {
+                    NSWindow.allowsAutomaticWindowTabbing = false
+                }
         }
         .windowStyle(.hiddenTitleBar)
         .windowResizability(.contentSize)
+        .commands {
+            CommandGroup(replacing: .newItem) { }
+            CommandGroup(replacing: .saveItem) { }
+            CommandGroup(replacing: .help) { }
+            CommandGroup(replacing: .textEditing) { }
+            CommandGroup(replacing: .windowSize) { }
+            CommandGroup(replacing: .sidebar) { }
+            CommandGroup(replacing: .systemServices) { }
+        }
     }
 }
